4074937f3220c2be54ca661d5b7ededac68dd045
Dinar Ishmulhametov <1nsayt@mail.ru>
2019-08-18 20:31:31 +0500

    added music

--- script.js
+++ script.js
@@ -1,31 +1,34 @@
-const searchForm = document.querySelector(".search-form"),
-  picture = document.querySelector(".picture-img");
+const picture = document.querySelector(".picture-img"),
+      picDescription = document.querySelector(".picture-description"),
+      picLikes = document.querySelector(".picture-likes");
 
-function apiSearch(e) {
-  e.preventDefault();
-  const searchText = document.querySelector(".search-input").value;
-  const server = `https://api.unsplash.com/photos/random/?query=${searchText}&client_id=a5abe91c63efb0b7b83d07e1a9abfd6c6a3e86307f6f1d1d748a0e2d7acd4cfb`;
+let delay = 5000;
+
+let timerId = setTimeout(function request() {
+  const server = `https://api.unsplash.com/photos/random/?&client_id=a5abe91c63efb0b7b83d07e1a9abfd6c6a3e86307f6f1d1d748a0e2d7acd4cfb`;
   requestApi(server);
-}
+  timerId = setTimeout(request, delay);
+}, delay);
 
-searchForm.addEventListener("submit", apiSearch);
 
 function requestApi(url) {
+
   const request = new XMLHttpRequest();
 
   request.open("GET", url);
+
   request.responseType = "json";
-  console.log(request);
+
   request.send();
 
   request.onload = () => {
     if (request.status != 200) {
-      console.log(`Ошибка ${request.status}: ${request.statusText || "status not found, use google"}`);
+      console.log(`Ошибка ${request.status}: ${request.statusText || "status not founded, use google"}`);
     } else {
-      let responseObj = request.response;
-      console.log(responseObj);
-      let url = responseObj.urls.thumb;
-      picture.src = url;
+      let {urls,description, likes} = request.response;
+      picture.style.backgroundImage = `url(${urls.thumb})`
+      picDescription.textContent = description || "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+      picLikes.textContent = `likes:${likes}`
     }
   };
 }
---
+++ scriptfm.js
@@ -0,0 +1,44 @@
+const genre = document.querySelector(".genre-select"),
+      input = document.querySelector(".genre-submit"),
+      list = document.querySelector(".topTen"),
+      btnClean = document.querySelector(".genre-clean");
+
+function apiSearch(e) {
+
+  e.preventDefault();
+
+  const searchText = genre.value;
+  if (searchText == "") {
+    return alert("please select genre");
+  }
+  const server = `http://ws.audioscrobbler.com/2.0/?limit=10&method=tag.gettoptracks&tag=${searchText}&api_key=b43ce99d4f1d944951e254318ecae510&format=json`;
+  
+  fetch(server)
+    .then(response => response.json())
+    .then(result => {
+      if (list.dataset.state) {
+        list.innerHTML = "";
+      }
+      let topTen = result.tracks.track,
+          title = document.createElement("h4");
+      title.textContent = `Top 10 of genre ${genre.value}`;
+      list.append(title);
+      topTen.map(item => {
+        let li = document.createElement("li"),
+           { url, name } = item;
+        li.insertAdjacentHTML(
+          "beforeend",
+          `<a href=${url}><p>${item.artist.name} - ${name}</p></a>`
+        );
+        list.append(li);
+        list.dataset.state = true;
+        genre.value = "";
+      });
+    })
+    .catch(error => console.error("Error:" + error.status));
+}
+
+input.addEventListener("click", apiSearch);
+btnClean.addEventListener("click",function(e){
+  e.innerHTML = "";
+})
